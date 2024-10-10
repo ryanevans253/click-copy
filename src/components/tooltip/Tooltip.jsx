d8d37b9ba9fe2db1f76daf192fa9007d1d005fa2
Ryan Evans <ryanevans253@yahoo.com>
2024-10-09 20:17:31 -0700

    Github link

--- src/components/tooltip/Tooltip.jsx
+++ src/components/tooltip/Tooltip.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Tooltip = () => {
   const message = `This app helps you efficiently copy and paste frequently used information, like your email, phone number, LinkedIn URL, and portfolio site, into job applications. Simply add your details once, click to copy them, and then paste wherever needed.
   No accounts or data storage required—just a straightforward tool with open-source code for transparency.
-  If you find this app useful, consider buying me a coffee to support further development!`;
+  If you find this app useful, consider buying me a coffee to support further development!
+  
+  Contributions welcome at https://github.com/ryanevans253/click-copy`;
 
   return (
     <div className="tooltip-button-container">
